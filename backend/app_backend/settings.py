28323a75a30baa31de1635f5aa022e30f578f764
Yash-g2310 <yg351124@gmail.com>
2025-11-01 16:20:54 +0530

    add: models for backend

--- backend/app_backend/settings.py
+++ backend/app_backend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Local apps
+    'rides',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom User Model
+AUTH_USER_MODEL = 'rides.User'
---
+++ backend/rides/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, DriverProfile, RideRequest
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    """Custom User admin"""
+    list_display = ['username', 'email', 'role', 'phone_number', 'completed_rides', 'is_active']
+    list_filter = ['role', 'is_staff', 'is_active']
+    search_fields = ['username', 'email', 'phone_number', 'google_id']
+    
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Additional Info', {
+            'fields': ('google_id', 'profile_picture', 'role', 'phone_number', 'completed_rides')
+        }),
+    )
+
+
+@admin.register(DriverProfile)
+class DriverProfileAdmin(admin.ModelAdmin):
+    """Driver Profile admin"""
+    list_display = ['user', 'vehicle_number', 'status', 'last_location_update']
+    list_filter = ['status']
+    search_fields = ['user__username', 'vehicle_number']
+    readonly_fields = ['last_location_update']
+
+
+@admin.register(RideRequest)
+class RideRequestAdmin(admin.ModelAdmin):
+    """Ride Request admin"""
+    list_display = ['id', 'passenger', 'driver', 'status', 'requested_at', 'accepted_at', 'completed_at']
+    list_filter = ['status', 'requested_at']
+    search_fields = ['passenger__username', 'driver__username', 'pickup_address']
+    readonly_fields = ['requested_at', 'accepted_at', 'started_at', 'completed_at', 'cancelled_at']
+    date_hierarchy = 'requested_at'
+
+
